8759625194a1c10d9c52302daee39667a4f00594
clementbo <clement.bourree.pro@gmail.com>
2026-02-26 15:34:13 +0100

    Fix: Settings.py for Static files

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
